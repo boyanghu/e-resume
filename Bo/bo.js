cb270e45794ab6c8384eaa7462f194cb54a8b69a
Bo Yang Hu <boyangh@gmail.com>
2013-05-01 11:21:35 -0700

    cool stuff

--- Bo/bo.js
+++ Bo/bo.js
@@ -7,9 +7,11 @@ function aboutbuttonOnClick(){
 }
 
 function hover(element){
+	document.getElementById("socialdispanel").style.visibility = "visible";
 	document.getElementById(element).style.opacity = "1";
 }
 
 function unhover(element){
 	document.getElementById(element).style.opacity = "0";
+	document.getElementById("socialdispanel").style.visibility = "collapse";
 }
