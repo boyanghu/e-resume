50982197ed0439b4195a62b7ac53c002e4daab7f
Bo Yang Hu <boyangh@gmail.com>
2013-05-01 01:36:43 -0700

    late night code II

--- Bo/bo.js
+++ Bo/bo.js
@@ -5,3 +5,11 @@ function closerOnClick(){
 function aboutbuttonOnClick(){
 	document.getElementById("aboutme").style.opacity ="1";
 }
+
+function hover(element){
+	document.getElementById(element).style.opacity = "1";
+}
+
+function unhover(element){
+	document.getElementById(element).style.opacity = "0";
+}
