be04da2628486a75dc4f18fce462c8ee40a850ec
Bo Yang Hu <boyangh@gmail.com>
2013-05-02 00:42:55 -0700

    late night commit

--- Bo/bo.js
+++ Bo/bo.js
@@ -13,3 +13,39 @@ function hover(element){
 function unhover(element){
 	document.getElementById(element).style.opacity = "0";
 }
+
+function facebookOnClick(){
+	document.getElementById("facebookpanel").style.visibility = "visible";
+	document.getElementById("facebookpanel").style.width = "50%";
+	document.getElementById("fbcloser").style.visibility = "visible";
+}
+
+function fpcloserOnClick(){
+	document.getElementById("fbcloser").style.visibility = "collapse";
+	document.getElementById("facebookpanel").style.visibility = "collapse";
+	document.getElementById("facebookpanel").style.width = "0%";
+}
+
+function twitterOnClick(){
+	document.getElementById("twitterpanel").style.visibility = "visible";
+	document.getElementById("twitterpanel").style.width = "50%";
+	document.getElementById("tcloser").style.visibility = "visible";
+}
+
+function tcloserOnClick(){
+	document.getElementById("tcloser").style.visibility = "collapse";
+	document.getElementById("twitterpanel").style.visibility = "collapse";
+	document.getElementById("twitterpanel").style.width = "0%";
+}
+
+function mailOnClick(){
+	document.getElementById("mailpanel").style.visibility = "visible";
+	document.getElementById("mailpanel").style.width = "50%";
+	document.getElementById("mcloser").style.visibility = "visible";
+}
+
+function mcloserOnClick(){
+	document.getElementById("mcloser").style.visibility = "collapse";
+	document.getElementById("mailpanel").style.visibility = "collapse";
+	document.getElementById("mailpanel").style.width = "0%";
+}
